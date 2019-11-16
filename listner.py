cac1e4532ee2ffb9bf9d2a411bf3d846ffb004ba
RohanSogani <ronsogani@gmail.com>
2019-11-15 17:11:22 -0800

    Initial Commit basic listner

---
+++ listner.py
@@ -0,0 +1,48 @@
+import time
+import threading
+
+import pyperclip
+
+def isUrl(url):
+    if url.startswith("http://") or url.startswith("https://"):
+        return True
+    return False
+
+def printOut(clipboard_content):
+    print("Found a url", str(clipboard_content))
+
+class copyUrlListner(threading.Thread):
+    def __init__(self, predicate, callback, pause=5.):
+        super(copyUrlListner, self).__init__()
+        self._predicate = predicate
+        self._callback = callback
+        self._pause = pause
+        self._stopping = False
+
+    def run(self):
+        currentCopyVal = ""
+        while not self._stopping:
+            temporaryCopyVal = pyperclip.paste()
+            if temporaryCopyVal != currentCopyVal:
+                currentCopyVal = temporaryCopyVal
+                if self._predicate(currentCopyVal):
+                    self._callback(currentCopyVal)
+            time.sleep(self._pause)
+
+    def stop(self):
+        self._stopping = True
+
+def main():
+    listner = copyUrlListner(isUrl, printOut, 5.)
+    listner.start()
+    while True:
+        try:
+            print("Patience, Waiting to be changed clipboard...")
+            time.sleep(10)
+        except KeyboardInterrupt:
+            listner.stop()
+            break
+
+
+if __name__ == "__main__":
+    main()
